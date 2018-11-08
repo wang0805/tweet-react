433265d19c7a393629f85e5e7fbb8e01a71f8ebf
wang0805 <wh_wang@live.com>
2018-11-08 14:45:58 +0800

    deliverables

--- script.js
+++ script.js
@@ -1,7 +1,43 @@
-var monkey = "hello";
+
+
+class ListItems extends React.Component {
+  render(){
+      let list = <li><span>{this.props.user.name}</span>&nbsp;<span>@{this.props.user.screen_name}</span></li>
+      let list2 = <li>{this.props.text}</li>
+      let list3; //have to declare variable first
+      if (this.props.entities.urls[0] != undefined){
+        let url = "https://www."+this.props.entities.urls[0].display_url;
+        list3 = <li><a href={url}>{this.props.entities.urls[0].display_url}</a></li>
+      }
+    
+      return (
+      <ul className="card">
+        {list}
+        {list2}
+        {list3}
+      </ul>
+    );
+  }
+}
+
+class Tweets extends React.Component {
+  render() {
+    let tweetelements = this.props.tweets.map(iterator => {
+      return <ListItems user={iterator.user} entities={iterator.entities} text ={iterator.text}/>
+    });
+
+    return (
+      <div>
+        {tweetelements}
+      </div>
+    )
+  }
+}
+
+
 ReactDOM.render(
-    <div>
-      {monkey}
+    <div class="container">
+      <Tweets tweets={tweets} />
     </div>,
     document.getElementById('root')
 );
